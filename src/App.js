d4d32baf87dd8bd16c684f87d847cdc3a4158414
mireu-san <67046863+mireu-san@users.noreply.github.com>
2021-11-17 23:05:54 +0900

    API integration process

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import './App.css';
 import { MenuItem, FormControl, Select } from '@mui/material';
 
 function App() {
-  const [countries, setCountries] = useState(['USA', 'UK', 'INDIA']);
-  
+  const [countries, setCountries] = useState([]);
+  useEffect(() => {
+      
+    const getCountriesData = async () => {
+        await fetch("https://disease.sh/v3/covid-19/countries")
+          .then((response) => response.json())
+          .then((data) => {
+            const countries = data.map((country) => ({
+                name: country.country,
+                value: country.countryInfo.iso2,
+            }));
+
+            setCountries(countries);
+
+          });
+        };
+        getCountriesData();
+      }, []);
+   
+
   return (
     <div className="app">
       <div className="app__header">
@@ -13,10 +31,9 @@ function App() {
       <h1>Covid-19 tracker</h1>
       <FormControl className="app__dropdown">
         <Select variant="outlined" value="abc">
-          {
-            countries.map((country) => (
-              <MenuItem value={country}>{country}</MenuItem>
-              ))}
+          {countries.map((country) => (
+              <MenuItem value={country.value}>{country.name}</MenuItem>
+            ))}
         </Select>
       </FormControl>
       </div>
