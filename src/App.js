53abfa3e10737f99a71adf3b5da961884cbe197e
mireu-san <67046863+mireu-san@users.noreply.github.com>
2021-11-17 17:52:23 +0900

    material ui adapted successfully

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import React from "react";
 import './App.css';
+import { MenuItem, FormControl, Select } from '@mui/material';
 
 function App() {
   return (
     <div className="app">
       <h1>Covid-19 tracker</h1>
+      <FormControl className="app__dropdown">
+        <Select variant="outlined" value="abc">
+          <MenuItem value="worldwide">Worldwide</MenuItem>
+          <MenuItem value="worldwide">Worldwide2</MenuItem>
+          <MenuItem value="worldwide">Worldwide3</MenuItem>
+          <MenuItem value="worldwide">Worldwide4</MenuItem>
+        </Select>
+      </FormControl>
+
     </div>
   );
 }
