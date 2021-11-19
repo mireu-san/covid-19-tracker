e6dee66e4160d3e185b9a4f5509abdd7e3502efd
mireu-san <67046863+mireu-san@users.noreply.github.com>
2021-11-19 18:13:13 +0900

    pause for sec - useEffect for executing App.js to display data at first

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React, { useState, useEffect } from "react";
 import './App.css';
-import { MenuItem, FormControl, Select } from '@mui/material';
+import { MenuItem, FormControl, Select, Card, CardContent, } from '@mui/material';
 import InfoBox from './InfoBox';
-
+import Map from './Map';
 
 function App() {
   const [countries, setCountries] = useState([]);
   const [country, setCountry] = useState('worldwide');
+  const [countryInfo, setCountryInfo] = useState({
+
+  });
+
+  // to display the data as soon as accessing the page
+  useEffect(() => {
+    fetch("https://disease.sh/v3/covid-19/all")
+      .then((response) => response.json())
+      .then((data) => {
+        setCountryInfo(data);
+        });
+  }, []); // [this blank square backet] allows - only execute itself only when I load App.js
 
 
   useEffect(() => {
@@ -27,21 +39,44 @@ function App() {
         getCountriesData();
       }, []);
    
-
+// GET API START
+// async and await fetch
 const onCountryChange = async (event) => {
   const countryCode = event.target.value;
-
-  console.log('test >>>', countryCode);
-
+  // console.log('test >>>', countryCode);
   setCountry(countryCode);
+
+  const url = 
+    countryCode === "worldwide"
+  ? 'https://disease.sh/v3/covid-19/all' 
+  : `https://disease.sh/v3/covid-19/countries/${countryCode}` // /v3/covid-19/countries/{country}/ -> get 'countryCode' specifically from it
+
+  await fetch(url)
+  .then(response => response.json())
+  .then(data => {
+    setCountry(countryCode);
+    setCountryInfo(data);
+  })
+
+  // https://disease.sh/v3/covid-19/all
+  // https://disease.sh/v3/covid-19/countries/[COUNTRY_CODE]
 };
+
+console.log("country info >>>" , countryInfo);
+
+// GET API section END
 
   return (
     <div className="app">
+      <div className="app__left">
       <div className="app__header">
         <h1>Covid-19 tracker</h1>
       <FormControl className="app__dropdown">
-        <Select variant="outlined" onChange={onCountryChange} value={country} >
+        <Select 
+          variant="outlined" 
+          onChange={onCountryChange} 
+          value={country} 
+        >
           <MenuItem value="worldwide">Worldwide</MenuItem>
           {countries.map((country) => (
               <MenuItem value={country.value}>{country.name}</MenuItem>
@@ -51,11 +86,21 @@ const onCountryChange = async (event) => {
       </div>
 
       <div className="app__stats">
-        <InfoBox title="Coronavirus Cases" cases={1234} total={1234} />
-        <InfoBox title="Recovered" cases={1234} total={1234} />
-        <InfoBox title="Deaths" cases={1234} total={1234} />
+        <InfoBox title="Coronavirus Cases" cases={countryInfo.todayCases} total={countryInfo.cases} />
+        <InfoBox title="Recovered" cases={countryInfo.todayRecovered} total={countryInfo.recovered} />
+        <InfoBox title="Deaths" cases={countryInfo.todayDeaths} total={countryInfo.deaths} />
     </div>
+
+      <Map />
     </div>
+    <Card className="app__right">
+      <CardContent>
+        <h3>Live cases by country</h3>
+        <h3>Worldwide new cases</h3>
+      </CardContent>
+    </Card>
+
+  </div>
   );
 }
 
