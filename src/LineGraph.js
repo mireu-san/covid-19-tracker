7210f0d45d0cdbc878f529842484288641368dec
mireu-san <67046863+mireu-san@users.noreply.github.com>
2021-11-20 20:19:14 +0900

    debugging - chart vertical error

--- src/LineGraph.js
+++ src/LineGraph.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react'
-import { Line } from 'react-chartjs-2';
-import numeral from 'numeral';
+import React, { useState, useEffect } from "react"
+import { Line } from "react-chartjs-2";
+import numeral from "numeral";
 
 // caution //
 const options = {
@@ -14,21 +14,21 @@ const options = {
     },
     maintainAspectRatio: false,
     tooltips: {
-        mode: 'index',
+        mode: "index",
         intersect: false,
         callbacks: {
             label: function(tooltipItem, data) {
-                return numeral(tooltipItem.value).format('+0,0');
+                return numeral(tooltipItem.value).format("+0,0");
             },
         },
     },
     scales: {
         xAxes: [
             {
-                type: 'time',
+                type: "time",
                 time: {
-                    format: 'MM/DD/YY',
-                    tooltipFormat: 'll',
+                    format: "MM/DD/YY",
+                    tooltipFormat: "ll",
                 },
             },
         ],
@@ -39,7 +39,7 @@ const options = {
                 },
                 ticks: {
                     callback: function(value, index, values) {
-                        return numeral(value).format('0a');
+                        return numeral(value).format("0a");
                     },
                 },
             },
@@ -48,56 +48,60 @@ const options = {
 };
 // caution end //
 
-const buildChartData = (data, casesType ='cases') => {
+const buildChartData = (data, casesType) => {
     let chartData = [];
     let lastDataPoint;
     // data.cases.forEach(date => 
     for(let date in data.cases) {
       if (lastDataPoint) {
-        const newDataPoint = {
+        let newDataPoint = {
           x: date,
           y: data[casesType][date] - lastDataPoint,
         };
           chartData.push(newDataPoint);
         }
-        lastDataPoint = data['cases'][date];
-      };
+        lastDataPoint = data[casesType][date];
+      }
       return chartData;
-  };
+};
 
 
-function LineGraph() {
+function LineGraph({ casesType }) { // purpose: if get source does not pass default cases...
     const [data, setData] = useState({});
     
 
     useEffect(() => {
         const fetchData = async () => {
-            await fetch('https://disease.sh/v3/covid-19/historical/all?lastdays=120')
-                .then(response => response.json())
+            await fetch("https://disease.sh/v3/covid-19/historical/all?lastdays=120")
+                .then(response => {
+                    return response.json();
+                })
                 .then(data => {
-                    let chartData = buildChartData(data, 'cases');
+                    let chartData = buildChartData(data, "cases");
                     setData(chartData);
                     console.log(chartData);
             })        
         };
-        fetchData();
+        fetchData(casesType);
     }, []);
 
     return (
+        // graph displays here
         <div>
             <h1>LineGraph is now displayed</h1>
-            {data?.length > 0 && (
+            {data?.length > 0 && ( // data? check even data exists e.g. instead of using data && data.lendth 
                 <Line 
                     options={options}
                     data={{
                         datasets: [
                             {
-                                backgroundColor: 'rgba(204, 16, 52, 0.5)',
-                                borderColor: '#CC1034',
+                                backgroundColor: "rgba(204, 16, 52, 0.5)",
+                                borderColor: "#CC1034",
                                 data: data,
                             },
                         ],
                     }}
+                    options={options}
                 />
             )}
         </div>
