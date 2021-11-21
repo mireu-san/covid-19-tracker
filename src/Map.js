20f0bdd5bd017c18fc8ca44f296a90c34c962058
mireu-san <67046863+mireu-san@users.noreply.github.com>
2021-11-21 17:47:01 +0900

    debugging - leaflet 11.21

--- src/Map.js
+++ src/Map.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import { MapContainer as LeafletMap, TileLayer } from 'react-leaflet';
 
 function Map() {
     return (
         <div className="map">
             <h1>this is Map</h1>
+            <LeafletMap>
+                <TileLayer
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+                />
+            </LeafletMap>
         </div>
     )
 }
